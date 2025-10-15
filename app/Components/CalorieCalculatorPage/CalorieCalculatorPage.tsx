eb0ca5766488ed6f228006f39f63c52f917523e4
ViniciusGNunes <vini.gn14@gmail.com>
2025-10-14 19:26:13 -0700

    Finished calorie calclator page form

--- app/Components/CalorieCalculatorPage/CalorieCalculatorPage.tsx
+++ app/Components/CalorieCalculatorPage/CalorieCalculatorPage.tsx
@@ -5,7 +5,7 @@ export default function CalorieCalculatorPage() {
     <>
       <div className="container">
         <div className="row">
-          <div className="col-8 col-xl-12 bg-dark text-white mt-5 mx-auto text-center rounded-4 p-3">
+          <div className="col-8 col-sm-12 col-xl-12 bg-dark text-white mt-5 mx-auto text-center rounded-4 p-3">
             <div className="row">
               <h1 className="mt-2">This is your calorie calculator</h1>
             </div>
@@ -24,7 +24,7 @@ export default function CalorieCalculatorPage() {
                   placeholder="70Kg"
                 />
                 <label htmlFor="floatingWeight" className="form-label">
-                  Insert your Weight(Kg)
+                  &nbsp;Insert your Weight(Kg)
                 </label>
               </div>
 
@@ -36,7 +36,7 @@ export default function CalorieCalculatorPage() {
                   placeholder="170cm"
                 />
                 <label htmlFor="floatingHeight" className="form-label">
-                  Insert your Height(cm)
+                  &nbsp;Insert your Height(cm)
                 </label>
               </div>
 
@@ -48,31 +48,60 @@ export default function CalorieCalculatorPage() {
                   placeholder="18y"
                 />
                 <label htmlFor="floatingAge" className="form-label">
-                  Insert your Age(Yrs)
+                  &nbsp;Insert your Age(Yrs)
                 </label>
               </div>
-              <div className="row my-4 mx-auto w-75">
-                <div className="form-check text-right border-white">
+              <div className="row my-3 mx-auto w-75">
+                <div className="col-6">
                   <input
-                    type="checkbox"
+                    type="radio"
                     className="btn-check"
+                    name="gender"
                     id="btn-check-Male"
+                    autoComplete="off"
                   />
-                  <label className="btn" htmlFor="btn-check-Male">
+                  <label
+                    className="btn btn-outline-light w-100"
+                    htmlFor="btn-check-Male"
+                  >
                     Male
                   </label>
                 </div>
-                <div className="form-check text-right border-white">
+
+                <div className="col-6">
                   <input
-                    type="checkbox"
+                    type="radio"
                     className="btn-check"
+                    name="gender"
                     id="btn-check-Female"
+                    autoComplete="off"
                   />
-                  <label className="btn" htmlFor="btn-check-Female">
+                  <label
+                    className="btn btn-outline-light w-100"
+                    htmlFor="btn-check-Female"
+                  >
                     Female
                   </label>
                 </div>
               </div>
+              <select className="form-select mb-3" aria-label="Activity Level">
+                <option selected> Choose your activity Level</option>
+                <option value="Sedentary">
+                  Sedentary - No physical activity
+                </option>
+                <option value="Light">
+                  Light - Light exercise 1-3 times per week
+                </option>
+                <option value="Moderate">
+                  Moderate - Moderate exercise 3-5 times per week
+                </option>
+                <option value="Intense">
+                  Intense - Heavy activity 6-7 times per week
+                </option>
+                <option value="Very Intense">
+                  Very Intense - Physical labour or exercises 2 times per day
+                </option>
+              </select>
 
               <button
                 className="btn btn-primary mx-auto w-75 mb-4"
